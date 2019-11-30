fa842d7d52c82e3ee499494c4de392452fea8afb
garcia <garcia@garciaui-MacBook-Pro.local>
2019-11-30 18:18:18 +0900

    12121222123311

--- lists/templatetags/on_favs.py
+++ lists/templatetags/on_favs.py
@@ -6,10 +6,10 @@ register = template.Library()
 
 @register.simple_tag(takes_context=True)
 def on_favs(context, room):
-   """ user = context.request.user
+    return
+    """ user = context.request.user
 
     # the_list = list_models.List.objects.get_or_none(
     the_list = list_models.List.objects.get(user=user, name="My Favourites Houses")
     return room in the_list.rooms.all()
-"""
-    pass
+    """
