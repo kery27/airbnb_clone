455a338b1d38ac687a72106c669d17791730fc11
garcia <garcia@garciaui-MacBook-Pro.local>
2019-12-02 01:35:22 +0900

    1201-1

--- config/settings.py
+++ config/settings.py
@@ -20,13 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "#m8vuz05k4@epzxv)w^h2ohlwqt%rn%($5$x$dhe01evo4a=df"
+SECRET_KEY = os.environ.get("DJANGO_SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # 개발인지 운영인지를 구분짓는 플래그래
 # 이값을 폴스로 바꾸면 404에러를 뱉고 트루면 어디가 에런지 알려주는것이라네
 
-DEBUG = True
+DEBUG = False
 
 # DEBUG = bool(os.environ.get("DEBUG"))
 
@@ -51,8 +51,8 @@ else:
         }
     }
 
-# ALLOWED_HOSTS = [".elasticbeanstalk.com", "localhost", "172.31.14.81"]
-ALLOWED_HOSTS = ["172d84d9.ngrok.io"]
+ALLOWED_HOSTS = [".elasticbeanstalk.com", "localhost", "172.31.14.81"]
+# ALLOWED_HOSTS = ["172d84d9.ngrok.io"]
 
 
 # Application definition
@@ -163,7 +163,7 @@ MEDIA_URL = "/media/"
 
 LOGIN_URL = "/users/login/"
 
-"""
+
 DEFAULT_FILE_STORAGE = "config.custom_storages.UploadStorage"
 STATICFILES_STORAGE = "config.custom_storages.StaticStorage"
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
@@ -175,4 +175,4 @@ AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
 
 AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
-"""
+
